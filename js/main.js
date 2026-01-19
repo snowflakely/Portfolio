1bd416578381761e71750217194f98332d5f47a5
snowflakely <ddangkongjam@gmail.com>
2026-01-19 09:52:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ $(function(){
     ]
     var portfolio_list = [
         ["순서번호","구분","제목","고객","완료시기","도구","범위","링크","이미지갯수"],
-        ["1","charcter design","saesik","-","2024","Adobe Photoshop","Graphic Design, Artwork,<br>Emoticon","","1"],
+        //["1","charcter design","saesik","-","2024","Adobe Photoshop","Graphic Design, Artwork,<br>Emoticon","","1"],
         ["2","logo design","tenz","Tenz","2023","Adobe Photoshop,<br>Adobe Illustrator","Graphic Design, Artwork,<br>Logo Design","","3"],
         ["3","logo design","hoinz","Hoinz","2024","Adobe Photoshop,<br>Adobe Illustrator","Graphic Design, Artwork,<br>Logo Design","","2"],
         ["4","logo design","jina_edu","Jina Edu","2024","Adobe Photoshop,<br>Adobe Illustrator","Graphic Design, Artwork,<br>Logo Design","","2"],
